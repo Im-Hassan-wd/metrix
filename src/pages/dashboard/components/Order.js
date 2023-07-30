66630061f35675e3b9ec3a6dcb1a0691f5a00f56
im-hassan-wd <alhassansahih@gmail.com>
2023-07-30 17:45:44 -0500

    order section

--- src/pages/dashboard/components/Order.js
+++ src/pages/dashboard/components/Order.js
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { orders } from "../../../utils/OrderData";
 export default function Order() {
   return (
     <div className="order">
       <h4>Recent orders</h4>
+      <ul>
+        {orders.map((order) => (
+          <li key={order.id}>
+            <img src={order.thumbnail} alt={order.name} />
+            <div>
+              <p className="name">{order.name}</p>
+              <p className="price">{order.price}</p>
+            </div>
+            <div>
+              <p className="date">{order.date}</p>
+              <p className="status">{order.status}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
